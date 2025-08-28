bcdb5eb453e930abde50711000a3639c92a49ef3
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-08-28 16:36:10 +0200

    feat(NextJS-AI-App): UI for Multiple Tools
    feat: add chat tools API and UI
    
    - Add multiple-tools API route with getLocation and getWeather tools
    - Implement UI for chat interactions with dynamic tool handling
    - Handle input/output states for tools in chat interface

---
+++ src/app/api/multiple-tools/route.ts
@@ -0,0 +1,66 @@
+import { openai } from "@ai-sdk/openai";
+import {
+	convertToModelMessages,
+	InferUITools,
+	stepCountIs,
+	streamText,
+	tool,
+	UIDataTypes,
+	UIMessage,
+} from "ai";
+import { z } from "zod";
+
+const tools = {
+	getLocation: tool({
+		description: "Get the location of the user",
+		inputSchema: z.object({
+			name: z.string().describe("The name of the user"),
+		}),
+		execute: async ({ name }) => {
+			if (name === "Bruce Wayne") {
+				return "Gotham City";
+			} else if (name === "Clark Kent") {
+				return "Metropolis";
+			} else {
+				return "Unkown";
+			}
+		},
+	}),
+	getWeather: tool({
+		description: "Get the current weather for a given location.",
+		inputSchema: z.object({
+			city: z.string().describe("The city to get the weather for."),
+		}),
+		execute: async ({ city }) => {
+			if (city === "Gotham") {
+				return "It's always cloudy in Gotham.";
+			} else if (city === "Metropolis") {
+				return "It's sunny in Metropolis.";
+			} else {
+				return `I don't have weather data for ${city}.`;
+			}
+		},
+	}),
+};
+
+export type Chatools = InferUITools<typeof tools>;
+export type ChatMessage = UIMessage<never, UIDataTypes, Chatools>;
+
+export async function POST(request: Request) {
+	try {
+		const { messages }: { messages: ChatMessage[] } = await request.json();
+
+		const result = streamText({
+			model: openai("gpt-4.1-nano"),
+			messages: convertToModelMessages(messages),
+			tools,
+			stopWhen: stepCountIs(3),
+		});
+
+		// Stream the response as a UI message stream
+		return result.toUIMessageStreamResponse();
+	} catch (error) {
+		console.error("Error streaming chat completion:", error);
+		return new Response("Failed to stream chat completion", { status: 500 });
+	}
+}
